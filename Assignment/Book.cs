9de230e9d2b370ddb256702fded1bd172ea45bf1
Ahmed El-Kholy <a.adel27111@gmail.com>
2025-02-13 04:53:46 +0200

    Q(a) Create User Defined Delegate with the same signature of methods existed in Bookfunctions class.

---
+++ Assignment/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+
+        public Book(string isbn, string title, string[] authors, DateTime publicationDate, decimal price)
+        {
+            ISBN = isbn;
+            Title = title;
+            Authors = authors;
+            Price = price;
+            PublicationDate = publicationDate;
+        }
+
+        public override string ToString()
+        {
+            string authorsList = string.Join(", ", Authors);
+            return $"ISBN: {ISBN}, Title: {Title}, Authors: {authorsList}, " +
+                $"Publication Date: {PublicationDate.ToShortDateString()}, Price: {Price}";
+        }
+
+    }
+
+    public class BookFunctions
+    {
+        public static string GetTitle(Book B)
+        {
+            return B.Title;
+        }
+
+        public static string GetAuthors(Book B)
+        {
+            return string.Join(", ", B.Authors);
+        }
+
+        public static string GetPrice(Book B)
+        {
+            return $"{B.Price}";
+        }
+    }
+}
---
+++ Assignment/LibraryEngine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    #region Q (a) Create User Defined Delegate with the same signature of methods existed in Bookfunctions class.
+
+    //public delegate string BookFunc(Book B);
+    //public class LibraryEngine
+    //{
+    //    public static void ProcessBooks(List<Book> blist, BookFunc fPtr)
+    //    {
+    //        if (blist.Count > 0 && fPtr is not null)
+    //        {
+    //            foreach (Book B in blist)
+    //            {
+    //                Console.WriteLine(fPtr(B));
+    //            }
+    //        }
+    //    }
+    //}
+
+    #endregion
+}
