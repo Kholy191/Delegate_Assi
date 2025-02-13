b2790e6b6bd7bdeb5cdf5531da8be36541b21142
Ahmed El-Kholy <a.adel27111@gmail.com>
2025-02-13 05:31:41 +0200

    Q(b) Use the Proper build in delegate.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -6,7 +6,7 @@
         {
 
             #region Q(a) Create User Defined Delegate with the same signature of methods existed in Bookfunctions class.
-            
+
             //List<Book> books = new List<Book>();
             //books.Add(new Book(
             //        isbn: "71",
@@ -17,6 +17,22 @@
             //        ));
 
             //BookFunc Func1 = BookFunctions.GetPrice;
+            //LibraryEngine.ProcessBooks(books, Func1);
+
+            #endregion
+
+            #region Q(b) Use the Proper build in delegate. 
+
+            //List<Book> books = new List<Book>();
+            //books.Add(new Book(
+            //        isbn: "71",
+            //        title: "Numbers",
+            //        authors: new string[] { "Ahmed", "Mahmoud" },
+            //        publicationDate: new DateTime(2010, 11, 22),
+            //        price: 233
+            //        ));
+
+            //Func<Book,string> Func1 = BookFunctions.GetPrice;
             //LibraryEngine.ProcessBooks(books, Func1);
 
             #endregion
